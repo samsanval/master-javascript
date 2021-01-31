50e4a2cec4a3afbd5cf196d52d37dd2dd300db46
Copado Notifications <copado.notifications@everis.com>
2021-01-31 22:33:29 +0100

    Tipos

--- js/05-tipos.js
+++ js/05-tipos.js
@@ -4,3 +4,17 @@ let numero1 = 8;
 let numero2 = 12;
 let operacion = numero2 + numero1;
 console.log(operacion);
+
+//Tipos de datos
+//Entero
+let numeroEntero = 2;
+//String
+let texto = "String";
+let textoComilla = 'Hola "que tal" las comillas';
+//Booleano
+let verdadero = true;
+let falso = false;
+let numeroString = "3";
+console.log(numeroString+7);
+let numeroFromString = Number(numeroString);
+console.log(numeroFromString+7);
