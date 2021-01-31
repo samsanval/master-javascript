a549a9d52a784633ffd438e9710688f69f7d1239
Copado Notifications <copado.notifications@everis.com>
2021-01-31 23:04:52 +0100

    Terminada seccion de ventanas. Empezando ejercicios

---
+++ js/10-ventanas.js
@@ -0,0 +1,9 @@
+'use strict'
+
+alert("Esta es mi alerta");
+
+let resultado = confirm("¿Quieres continuar");
+console.log(resultado);
+
+let edad=prompt("Introduzca su edad",18);
+console.log(typeof(edad));
